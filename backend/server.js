15c6d4e8a35ec8085572b9614260dc3cd2c5824f
Nikitarathod2001 <nikitarathod3601@gmail.com>
2025-11-28 08:50:44 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/FoodRouter.js";
+import userRouter from "./routes/userRouter.js";
+import cartRouter from "./routes/cartRouter.js";
+import orderRouter from "./routes/orderRouter.js";
+
+// app config
+const app = express();
+const PORT = 4000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+// db connection
+connectDB();
+
+// API endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Enjoy fresh, delicious food delivered right to your doorstep. Our food delivery service brings you a wide variety of meals from your favorite restaurants—fast, hot, and hassle-free.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
