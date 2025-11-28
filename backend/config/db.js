ad32f1c8634e6c59dbc2aac54da7636e1514de10
Nikitarathod2001 <nikitarathod3601@gmail.com>
2025-11-28 09:05:54 +0530

    Update db.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://nikitarathod3601_db_user:nikita3636@cluster0.w4xkimp.mongodb.net/food-del").then(() => console.log("DB Connected"));
+  await mongoose.connect(process.env.MONGODB_URI).then(() => console.log("DB Connected"));
 };
